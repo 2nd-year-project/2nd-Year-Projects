9cdb6cf09c88e2e41104cc2f86307f21e0feea03
MAHMOUD AFIFY <MAHMOUD AFIFY@LAPTOP-O5V2NMQI>
2019-04-03 17:28:06 +0200

    mahmoud try

---
+++ src/oop_project/try_git.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_project;
+
+/**
+ *
+ * @author MAHMOUD AFIFY
+ */
+public class try_git {
+    int x,y;
+    public try_git(){
+    x=y=0;
+    }
+           
+    
+}
